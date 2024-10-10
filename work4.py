089a30b1c64e6a20ac1e2a226760f8c2fa2b190f
admin <maksim.d05@mail.ru>
2024-10-10 22:20:43 +0300

    new file:   work4.py

---
+++ work4.py
@@ -0,0 +1,13 @@
+# Задача 2
+# s = 'dfgsd(gsdfgs)fgsdfgsfgsfg' 
+# print(s[s.index('(')+1:s.index(')')])
+# Задача 3
+# s = 'оврпаоаилоряорягвармяомтряоваяюмяоаврмюявамо'
+# in_я = []
+# for n, i in enumerate(s):
+#     if i == 'я':
+#         for j in range(n, len(s)):
+#             if s[j] == 'а' and j - n > 1:
+#                 in_я.append([n, j])
+# for i in in_я:
+#     print(s[i[0]+1:i[1]])
