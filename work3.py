f429a1d6b6da4dcd4dda39e7bbb67cd69281583e
admin <maksim.d05@mail.ru>
2024-10-10 21:42:36 +0300

    new file:   work3.py

---
+++ work3.py
@@ -0,0 +1,11 @@
+# Задача 1
+# n = int(input())
+# t = 0
+# for i in range(n+1):
+#     t += i*i*i
+# print(t)
+# Задача 2
+# for i in range (1,10):
+#     for a in range (1,10):
+#         print(f'{i * a : 3}', end=' ')
+#     print()
