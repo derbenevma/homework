9fcb8315a349b2411d979026a270b2602126d736
admin <maksim.d05@mail.ru>
2024-10-17 23:20:54 +0300

    new file:   work5.py

---
+++ work5.py
@@ -0,0 +1,41 @@
+# задача 1
+# def acos(x, y) :
+#     return x  / ((x * x + y * y) ** 0.5)
+ 
+# x1, x2 = map(float,input().split())
+# y1, y2 = map(float,input().split())
+# z1, z2 = map(float,input().split())
+# res = [x1, x2]
+# acosx = acos(x1, x2)
+# acosy = acos(y1, y2)
+# if acosy > acosx :
+#     acosx = acosy
+#     res = [y1, y2]
+# acosz = acos(z1, z2)
+# if acosz > acosx :
+#     acosz = acosz
+#     res = [z1, z2]
+# print(*res)
+# задача 2
+# n = int(input())
+ 
+# lst = [True for _ in range(n+1)]
+ 
+# i = 1
+# while 2*i*(i + 1) < n:
+#     j = i
+#     while j <= (n - i) / (2*i + 1):
+#         lst[2*i*j + i + j] = False
+#         j = j + 1
+#     i = i + 1
+ 
+# for i in range(1, n+1):
+#     elem = lst[i]
+#     if elem:
+#         prime = 2*i + 1
+#         if prime > n: break
+#         a = bin(prime)[2:]
+#         b = bin(prime)[2:][::-1]
+ 
+#         if a == b:
+#             print(prime, end=' ')
