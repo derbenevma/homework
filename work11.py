866b2a49cd83fb3c2aa24242842c70fae2b25130
derbenev.ma <maksim.d05@mail.ru>
2024-12-13 00:13:10 +0300

    Update work11.py

--- work11.py
+++ work11.py
@@ -1,12 +1,13 @@
+# Задание 3
 import pandas as pd
+df = pd.read_csv("NISPUF17.csv", sep=',')
+dfMale = df[df["SEX"] == 1]
+dfFemale = df[df["SEX"] == 2]
 
-def chickenpox_by_sex(data):
-  grouped = data.groupby('sex')
-  vaccinated_counts = grouped['vaccine'].sum()
-  chickenpox_counts = grouped['chickenpox'].sum()
-  ratios = chickenpox_counts / vaccinated_counts
-  return ratios.to_dict()
+maleCpx = dfMale[(dfMale["HAD_CPOX"] == 1) & (dfMale["P_NUMVRC"] > 0)].shape[0]/dfMale[(dfMale["HAD_CPOX"] == 2) & (dfMale["P_NUMVRC"] > 0)].shape[0]
+femaleCpx = dfFemale[(dfFemale["HAD_CPOX"] == 1) & (dfFemale["P_NUMVRC"] > 0)].shape[0]/dfFemale[(dfFemale["HAD_CPOX"] == 2) & (dfFemale["P_NUMVRC"] > 0)].shape[0]
 
-data = pd.read_csv('chickenpox_data.csv')
-ratios = chickenpox_by_sex(data)
-print(ratios)
+print(
+    {"male": round(maleCpx, 4),
+        "female": round(femaleCpx, 4)}
+)
