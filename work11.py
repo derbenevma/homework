649bd3a1f00b48bf850167a3fabd672490f7422a
derbenev.ma <maksim.d05@mail.ru>
2024-12-13 00:10:07 +0300

    Update work11.py

--- work11.py
+++ work11.py
@@ -1,1 +1,12 @@
+import pandas as pd
 
+def chickenpox_by_sex(data):
+  grouped = data.groupby('sex')
+  vaccinated_counts = grouped['vaccine'].sum()
+  chickenpox_counts = grouped['chickenpox'].sum()
+  ratios = chickenpox_counts / vaccinated_counts
+  return ratios.to_dict()
+
+data = pd.read_csv('chickenpox_data.csv')
+ratios = chickenpox_by_sex(data)
+print(ratios)
