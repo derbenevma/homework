cf7db59c645f3c0821bc2ef9dcdaa64aa2317c15
derbenev.ma <maksim.d05@mail.ru>
2025-03-26 14:31:34 +0300

    Create Homework2.py

---
+++ Homework2.py
@@ -0,0 +1,21 @@
+# class Recipe:
+#     def __init__(self, name, ingredients):
+#         self.name = name
+#         self.ingredients = ingredients
+
+#     def print_ingredients(self):
+#         print(f"Ингредиенты для {self.name}:")
+#         for ingredient in self.ingredients:
+#             print(f"- {ingredient}")
+
+#     def cook(self):
+#         print(f"Сегодня мы готовим {self.name}.")
+#         print(f"Выполняем инструкцию по приготовлению блюда {self.name}...")
+#         print(f"Блюдо {self.name} готово!")
+
+# spaghetti = Recipe("Спагетти болоньезе", ["Спагетти", "Фарш", "Томатный соус", "Лук", "Чеснок", "Соль"])
+# spaghetti.print_ingredients()
+# spaghetti.cook()
+# cake = Recipe("Кекс", ["Мука", "Яйца", "Молоко", "Сахар", "Сливочное масло", "Соль", "Ванилин"])
+# cake.print_ingredients()
+# cake.cook()
