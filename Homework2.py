5b880453b40c7a78ec0df0d204b51ffcd8916806
derbenev.ma <maksim.d05@mail.ru>
2025-03-27 12:55:52 +0300

    Update Homework2.py

--- Homework2.py
+++ Homework2.py
@@ -19,3 +19,28 @@ spaghetti.cook()
 cake = Recipe("Кекс", ["Мука", "Яйца", "Молоко", "Сахар", "Сливочное масло", "Соль", "Ванилин"])
 cake.print_ingredients()
 cake.cook()
+
+# 3. Класс "Кэширующий калькулятор"
+class CacheCalculator:
+    def __init__(self):
+        self._cache = {}
+
+    def __call__(self, x):
+        if not isinstance(x, (int, float)):
+            raise TypeError("Аргумент должен быть числом")
+
+        if x not in self._cache:
+            self._cache[x] = x * x
+
+        return self._cache[x]
+
+    @property
+    def cache(self):
+        return self._cache
+
+# Примеры проверки (раскомментируйте, чтобы протестировать)
+# calc = CacheCalculator()
+# calc(2)  # 4, кэш {2:4}
+# calc(2.5)  # 6.25, кэш {2:4, 2.5:6.25}
+# calc("text")  # TypeError
+# print(calc.cache)  # {'2':4, ...}
