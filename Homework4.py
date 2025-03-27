b6cce9452a71347dcae303b760f8eac4837ae5f6
derbenev.ma <maksim.d05@mail.ru>
2025-03-27 12:14:02 +0300

    Update Homework4.py

--- Homework4.py
+++ Homework4.py
@@ -1,27 +1,27 @@
 # №1
-# class Car:
-#     def __init__(self, make, model):
-#         self.make = make
-#         self.model = model
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
 
-#     def __getattr__(self, item):
-#         return "This attribute is not available"
-# c = Car("Toyota", "Corolla")
-# print(c.make) 
-# print(c.color)
-# №2
-# class Rectangle:
-#     def __init__(self, width, height):
+    def __getattr__(self, item):
+        return "This attribute is not available"
+c = Car("Toyota", "Corolla")
+print(c.make) 
+print(c.color)
+№2
+class Rectangle:
+    def __init__(self, width, height):
 
-#         self.width = width
-#         self.height = height
+        self.width = width
+        self.height = height
 
-#     def __setattr__(self, name, value):
-#         if name not in ('width', 'height'):
-#             raise AttributeError("Local attributes are not allowed")
-#         super().__setattr__(name, value)
+    def __setattr__(self, name, value):
+        if name not in ('width', 'height'):
+            raise AttributeError("Local attributes are not allowed")
+        super().__setattr__(name, value)
 
-# r = Rectangle(10, 20)
-# r.width = 15
-# r.height = 25
-# r.color = 'red'
+r = Rectangle(10, 20)
+r.width = 15
+r.height = 25
+r.color = 'red'
