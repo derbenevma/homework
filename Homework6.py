88ff46aab5e6e1a8e9bd703f19e405b1b1bfc5b3
derbenev.ma <maksim.d05@mail.ru>
2025-03-27 12:12:57 +0300

    Update Homework6.py

--- Homework6.py
+++ Homework6.py
@@ -1,38 +1,38 @@
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
-# class Derivative:
+class Derivative:
 
-#     def __init__(self, func):
-#         self.func = func
-#         self.h = 1e-5
+    def __init__(self, func):
+        self.func = func
+        self.h = 1e-5
 
-#     def __get__(self, instance, owner):
-#         return self  
+    def __get__(self, instance, owner):
+        return self  
 
-#     def __call__(self, x):
-#         return (self.func(x + self.h) - self.func(x - self.h)) / (2 * self.h)
+    def __call__(self, x):
+        return (self.func(x + self.h) - self.func(x - self.h)) / (2 * self.h)
 
 
-# class ExponentialFunction:
+class ExponentialFunction:
 
-#     def __init__(self, a):
-#         self.a = a
-#         self.derivative = Derivative(self)  
+    def __init__(self, a):
+        self.a = a
+        self.derivative = Derivative(self)  
 
-#     def __call__(self, x):
-#         return self.a * np.exp(x)
+    def __call__(self, x):
+        return self.a * np.exp(x)
 
-# func = ExponentialFunction(2)
-# x = np.linspace(-2, 2, 100)
-# y = [func(xi) for xi in x]
-# dy = [func.derivative(xi) for xi in x]
+func = ExponentialFunction(2)
+x = np.linspace(-2, 2, 100)
+y = [func(xi) for xi in x]
+dy = [func.derivative(xi) for xi in x]
 
-# plt.plot(x, y, label='f(x)')
-# plt.plot(x, dy, label='f\'(x)')
-# plt.xlabel('x')
-# plt.ylabel('y')
-# plt.title('Экспоненциальная функция и её производная')
-# plt.legend()
-# plt.grid(True)
-# # plt.show()
+plt.plot(x, y, label='f(x)')
+plt.plot(x, dy, label='f\'(x)')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Экспоненциальная функция и её производная')
+plt.legend()
+plt.grid(True)
+# plt.show()
