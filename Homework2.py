ca02b064cb9252fed46c9ec6da9d9b6954a05e21
derbenev.ma <maksim.d05@mail.ru>
2025-03-27 12:12:24 +0300

    Update Homework2.py

--- Homework2.py
+++ Homework2.py
@@ -1,21 +1,21 @@
-# class Recipe:
-#     def __init__(self, name, ingredients):
-#         self.name = name
-#         self.ingredients = ingredients
+class Recipe:
+    def __init__(self, name, ingredients):
+        self.name = name
+        self.ingredients = ingredients
 
-#     def print_ingredients(self):
-#         print(f"Ингредиенты для {self.name}:")
-#         for ingredient in self.ingredients:
-#             print(f"- {ingredient}")
+    def print_ingredients(self):
+        print(f"Ингредиенты для {self.name}:")
+        for ingredient in self.ingredients:
+            print(f"- {ingredient}")
 
-#     def cook(self):
-#         print(f"Сегодня мы готовим {self.name}.")
-#         print(f"Выполняем инструкцию по приготовлению блюда {self.name}...")
-#         print(f"Блюдо {self.name} готово!")
+    def cook(self):
+        print(f"Сегодня мы готовим {self.name}.")
+        print(f"Выполняем инструкцию по приготовлению блюда {self.name}...")
+        print(f"Блюдо {self.name} готово!")
 
-# spaghetti = Recipe("Спагетти болоньезе", ["Спагетти", "Фарш", "Томатный соус", "Лук", "Чеснок", "Соль"])
-# spaghetti.print_ingredients()
-# spaghetti.cook()
-# cake = Recipe("Кекс", ["Мука", "Яйца", "Молоко", "Сахар", "Сливочное масло", "Соль", "Ванилин"])
-# cake.print_ingredients()
-# cake.cook()
+spaghetti = Recipe("Спагетти болоньезе", ["Спагетти", "Фарш", "Томатный соус", "Лук", "Чеснок", "Соль"])
+spaghetti.print_ingredients()
+spaghetti.cook()
+cake = Recipe("Кекс", ["Мука", "Яйца", "Молоко", "Сахар", "Сливочное масло", "Соль", "Ванилин"])
+cake.print_ingredients()
+cake.cook()
